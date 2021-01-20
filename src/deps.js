a311f464427f8a1a34a71b3d51644a54ece24fee
Miqueias Rodrigues da Costa <miqsoftware1@hotmail.com>
2021-01-20 09:25:23 -0300

    adicionado dependencia de jwt

--- src/deps.js
+++ src/deps.js
@@ -1,3 +1,5 @@
 export { Application, Router, Context} from "https://deno.land/x/oak/mod.ts";
 export { applyGraphQL, gql, GQLError } from "https://deno.land/x/oak_graphql/mod.ts";
 export { Client } from "https://deno.land/x/postgres/mod.ts";
+export { create, verify } from "https://deno.land/x/djwt/mod.ts"
+export { UserInput } from './graphql/Schema/userInput.js' 
