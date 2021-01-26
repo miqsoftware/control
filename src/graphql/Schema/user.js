905de162b9a3b4211ec37d1df2f43f5cdc0df401
Miqueias Rodrigues da Costa <miqsoftware1@hotmail.com>
2021-01-26 08:11:35 -0300

    gerando chave token para os usuarios ADMIN

--- src/graphql/Schema/user.js
+++ src/graphql/Schema/user.js
@@ -14,39 +14,40 @@ const User = `
 `;
 
 async function fetchDataUser(name) {
-    try {
         const result = await db.query({
             text: 'SELECT * FROM "user" WHERE nome = $1',
             args: [name]
         });    
         return result.rowsOfObjects()[0];        
-    } catch (error) {
-        throw new GQLError('ERRORshow: ' + error);
-    }    
 }
 
 const user = async (_, { input: { name, password } },  { token }) => {
-   
-        const userData = await fetchDataUser(name); 
-        if (userData){
-            if (userData.senha === password && userData.nome === name){
-                const jwt = await createToken(name);
-                   
-                const obj = {
-                    ...userData,
-                    token
-                }
-                 return {
-                    ...userData,
-                    token
-                }
-            }else{
-                throw new GQLError({ text: 'invalid name or password' })
+    const userData = await fetchDataUser(name);
+    const token_aux = await createToken(name); 
+    console.log(token_aux);
+    
+        try {
+            if (await verifyToken(token)){
+                console.log('token-ok');
             }
+        } catch (error) {
+            throw new GQLError({message: 'Token expired or invalid'})  
+        }  
+
+    if (userData){
+        if (userData.senha === password && userData.nome === name){
+                const jwt = (userData.perfil === 'ADMIN') ? await createToken(name) : null;
+            const objUser = {
+                ...userData,
+                token: jwt
+            }
+            return objUser
         }else{
-            throw new GQLError({ text: 'user not found.' })
+            throw new GQLError({ text: 'invalid name or password' })
         }
-        
+    }else{
+        throw new GQLError({ text: 'user not found.' })
+    }        
  }
 
 export {
