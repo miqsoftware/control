be4d821a7c3f5a6aa228a74723d675d7b6a7795f
Miqueias Rodrigues da Costa <miqsoftware1@hotmail.com>
2021-01-26 08:06:04 -0300

    retirado as linhas de codigo de try-catch

--- src/token/generator.js
+++ src/token/generator.js
@@ -1,4 +1,5 @@
-import { create, verify } from '../deps.js'
+
+import { create, verify, GQLError } from '../deps.js'
 const secret = 'miqueias'
 const header = {
     alg: 'HS512',
@@ -12,21 +13,13 @@ export async function createToken(info){
         info,
         exp: Date.now() / 1000 + 300,
     }
-    try {
         return await create(header, payload, secret)
         .then( response => {
             return response                
-   })
-    } catch (error) {
-        return error
-    }
+        })
 }
 
 export async function verifyToken(token){
-    try {
-        return await verify( token, secret, 'HS512' )
-    } catch (error) {
-        return error        
-    }
+    return await verify( token, secret, 'HS512' )
 }
 
